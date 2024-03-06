c3f94b308be1dd5f4679b30ed3a9df5a44765df7
Jimmy <jimmygam@icloud.com>
2024-03-06 15:16:10 +0100

    Update test_cartepizzeria.py

--- test_cartepizzeria.py
+++ test_cartepizzeria.py
@@ -2,30 +2,38 @@ import unittest
 from unittest.mock import Mock
 from cartepizzeria import CartePizzeria
 
-def test_is_not_empty():
-    pizza = Mock()
-    cp = CartePizzeria()
-    cp.pizzas = [pizza]
-    assert not cp.is_empty()
+class TestCartePizzeria(unittest.TestCase):
 
-def test_nb_pizzas():
-    pizza = Mock()
-    cp = CartePizzeria()
-    cp.pizzas = [pizza]
-    assert cp.nb_pizzas() == 1
+    def test_is_not_empty(self):
+        pizza = Mock()
+        cp = CartePizzeria()
+        cp.pizzas = [pizza]
+        assert not cp.is_empty()
 
-def test_add_pizza():
-    pizza = Mock()
-    cp = CartePizzeria()
-    cp.pizzas = [pizza]
-    cp.add_pizza(pizza)
-    assert cp.nb_pizzas() == 2
+    def test_nb_pizzas(self):
+        pizza = Mock()
+        cp = CartePizzeria()
+        cp.pizzas = [pizza]
+        assert cp.nb_pizzas() == 1
 
-def test_remove_pizza():
-    pizza = Mock()
-    pizza2 = Mock()
-    cp = CartePizzeria()
-    cp.pizzas = [pizza]
-    cp.add_pizza(pizza2)
-    cp.remove_pizza("pizza2")
-    assert cp.nb_pizzas() == 1
+    def test_add_pizza(self):
+        pizza = Mock()
+        cp = CartePizzeria()
+        self.assertTrue(len(cp.pizzas) == 0)
+        cp.add_pizza(pizza)
+        self.assertTrue(len(cp.pizzas) == 1)
+
+    def test_remove_pizza(self):
+        pizza = Mock()
+        pizza.name = "calzone"
+        pizza2 = Mock()
+        pizza2.name = "canibale"
+        cp = CartePizzeria()
+        cp.pizzas = [pizza]
+        cp.add_pizza(pizza2)
+        self.assertTrue(len(cp.pizzas) == 2)
+        cp.remove_pizza("canibale")
+        self.assertTrue(len(cp.pizzas) == 1)
+    
+if __name__ == "__main__":
+    unittest.main()
